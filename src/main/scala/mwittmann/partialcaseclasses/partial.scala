b99f2ad08f87b9106e1687cb0c4090c53ecb03a7
Max Wittmann <damxam@gmail.com>
2017-09-03 13:53:46 +1000

    Cleanup

--- src/main/scala/mwittmann/partialcaseclasses/partial.scala
+++ src/main/scala/mwittmann/partialcaseclasses/partial.scala
@@ -21,49 +21,25 @@ object Explorer {
   }
 }
 
-
-
 object PartialDefnO {
 
   def collectParams(annotationStat: Stat): Seq[(String, Seq[String])] = {
     val q"new partial(..$terms)" = annotationStat
-    println("Terms are:\n" + Explorer.explore(terms))
 
     val partials: Seq[(String, Seq[Term.Arg])] = terms.map {
-//      case q"PartialDefn($name, ..$terms)" => (name.value, terms)
-//      case q"PartialDefn($name, ..$terms)" => {
-      case q"PartialDefn($name, List(..$terms))" => {
-        println(s"Name is ${Explorer.explore(name)}")
-
-        val x: (String, immutable.Seq[Term.Arg]) =
-          name match {
-            case n: scala.meta.Lit if n.value.isInstanceOf[String] => (n.value.asInstanceOf[String], terms)
-            case n: scala.meta.Lit => throw new Exception(s"Cannot use type ${n.getClass()} as name of case class. Must string!")
-            //case t"$n" => (n.`, terms)
-            case bad => throw new Exception (s"Failed to match to literal: ${Explorer.explore(bad)}")
-          }
-        x
-      }
-
-      //case bad => throw new Exception(s"Don't know how to use $bad\n${Explorer.explore(bad)}")
-      case bad => {
-        bad match {
-          case q"PartialDefn(<$name>, $any)" => throw new Exception("Matched 1")
-          case q"PartialDefn($any)"  => throw new Exception("Matched 2")
-          case q"PartialDefn($any1, ..$any2)"  => throw new Exception("Matched 2b")
-          case q"PartialDefn($any1, $any2)"  => throw new Exception("Matched 2c")
-          case q"PartialDefn(..$any)"  => throw new Exception("Matched 2d")
-          case q"$name($any)"  => throw new Exception("Matched 3")
-          case q"<$name>(..$any)"  => throw new Exception("Matched 4")
-          case q"$name(..$any)"  => throw new Exception("Matched 5")
-          //case q"<PartialDefn>($any)"  => throw new Exception("Matched 2")
-          case _  => throw new Exception("Matched 6")
+      case q"PartialDefn($name, List(..$terms))" =>
+        name match {
+          case n: scala.meta.Lit if n.value.isInstanceOf[String] => (n.value.asInstanceOf[String], terms)
+          case n: scala.meta.Lit => throw new Exception(s"Cannot use type ${n.getClass()} as name of case class. Must string!")
+          case bad => throw new Exception (s"Failed to match to literal: ${Explorer.explore(bad)}")
         }
-      }
+
+      case bad => throw new Exception(s"Don't know how to use $bad\n${Explorer.explore(bad)}")
     }
 
-    val r = partials.map(v =>
-      (v._1,
+    partials.map(v =>
+      (
+        v._1,
         v._2.collect {
           case s: Lit if s.value.isInstanceOf[String] => s.value.asInstanceOf[String]
           case s: Lit => throw new Exception(s"Expected String, got ${s.value}")
@@ -71,7 +47,6 @@ object PartialDefnO {
         }
       )
     )
-    r
   }
 }
 
@@ -87,31 +62,15 @@ class partial(args: List[PartialDefn]) extends StaticAnnotation {
     val derivedClasses = paramNames.map { case (name, vars) =>
       val varsSet = vars.toSet
       val derivedParams = params.filter(p => varsSet.contains(p.name.value))
-      val newName: Type.Name = Type.Name(name)
-      val x: Defn.Class = q"case class $newName (..$derivedParams)"
-      x
+      val newName = Type.Name(name)
+      q"case class $newName (..$derivedParams) extends $template"
     }
 
     val derivedClasses2: immutable.Seq[Stat] = derivedClasses.map(_.asInstanceOf[Stat]).to[immutable.Seq]
 
-//    q"""case class $tName (..$params) {
-//        ${derivedClasses}
-//      }
-//     """
-
-    val x = q"""case class $tName (..$params) {
+    q"""case class $tName (..$params) {
         ..$derivedClasses2
       }
      """
-
-    x
-
-    //    val derivedParams = params.filter(p => paramNames.contains(p.name.value))
-//    val newName = tName.copy(tName.value + "_derived")
-
-//    q"""case class $tName (..$params) {
-//        case class $newName (..$derivedParams)
-//      }
-//     """
   }
 }
